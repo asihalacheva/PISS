429ee6a8459dfa9b2e86ae92cef215e76384acfa
stnssllv <106099288+stnssllv@users.noreply.github.com>
2023-01-15 15:05:13 +0200

    add more data

--- back-end/db/create/tour.sql
+++ back-end/db/create/tour.sql
@@ -14,9 +14,8 @@ CREATE TABLE `users` (
 
 
 INSERT INTO `users` (`id`, `username`, `password`, `full_name`, `email`) VALUES
-(1, 'stanislgi1', '$2y$10$foJL0YFlm0kJKjQVbVDl3O0ZT1nzfagpeYG4Qg0efhmaKeluVmEJW', 'Stanislava Ivanova', 'stanislgi1@uni-sofia.bg'), 
-(2, 'arhalachev', '$2y$10$fek8734Ztplf9O6GWqmz..Q4vqxQIW/1iu63kxLmiq.E80NgU9Uj.', 'Asibe Halacheva', 'arhalachev@uni-sofia.bg'),
-(3, 'stnssllv', '$2y$10$4BGQmbj7Zts2w2yJFzrpgeHv/H0JZBFGetG87YTBS0HB3Gj.uY2ji', 'St Iv', 'stanissllava.iv@gmail.com');
+(1, 'arhalachev', '$2y$10$fek8734Ztplf9O6GWqmz..Q4vqxQIW/1iu63kxLmiq.E80NgU9Uj.', 'Asibe Halacheva', 'arhalachev@uni-sofia.bg'),
+(2, 'stanislgi1', '$2y$10$4BGQmbj7Zts2w2yJFzrpgeHv/H0JZBFGetG87YTBS0HB3Gj.uY2ji', 'Stanislava Ivanova', 'stanislgi1@uni-sofia.bg');
 
 
 CREATE TABLE `destinations` (
@@ -36,7 +35,10 @@ INSERT INTO `destinations` (`id`, `dest_id`, `name_dest`, `price`, `departure`, 
 (4, 4, 'Париж', 400, '2023-05-25', 'https://media.istockphoto.com/id/1280246120/photo/eiffel-tower-in-paris-skyline-at-dawn.jpg?b=1&s=170667a&w=0&k=20&c=aKos9h3WQiUJoUIfH4bXhA9frvUzPYMnk5KyBMdjeNQ='),
 (5, 5, 'Сингапур', 910, '2023-02-18', 'https://media.istockphoto.com/id/491056644/photo/singapore-skyline-cityscape-at-night.jpg?s=612x612&w=0&k=20&c=kdXYIXNV-8WM3ZOeBCS2JLgrqbTbV6YUWDFIHFThwwk='),
 (6, 6, 'Венеция', 450, '2023-03-09', 'https://media.istockphoto.com/id/1388018793/photo/grand-canal-in-venice.jpg?b=1&s=170667a&w=0&k=20&c=OFgzwNIGVwewTC21JtOecQiFMCg1wlp7cuQkR_UxEHU='),
-(7, 1, 'Дубай', 510, '2023-02-06', 'https://media.istockphoto.com/id/1333035210/photo/sunset-view-of-the-dubai-marina-and-jbr-area-and-the-famous-ferris-wheel-and-golden-sand.jpg?b=1&s=170667a&w=0&k=20&c=cYgbQF26FiE9SvkaFv6dmmZLh1WM7ZG7iuGpzEsZktE=');
+(7, 1, 'Дубай', 510, '2023-02-06', 'https://media.istockphoto.com/id/1333035210/photo/sunset-view-of-the-dubai-marina-and-jbr-area-and-the-famous-ferris-wheel-and-golden-sand.jpg?b=1&s=170667a&w=0&k=20&c=cYgbQF26FiE9SvkaFv6dmmZLh1WM7ZG7iuGpzEsZktE='),
+(8, 4, 'Париж', 390, '2023-04-04', 'https://media.istockphoto.com/id/1176360891/photo/cityscape-of-paris.jpg?b=1&s=170667a&w=0&k=20&c=HtqAoaTmrqg5lE1r_VLnGVFDTV-1vKhnGm_3pV6MtcE='),
+(9, 1, 'Дубай', 770, '2023-04-30', 'https://media.istockphoto.com/id/1161117789/photo/camel-tourists-caravan-walking-on-sunset-desert-near-dubai-skyline.jpg?b=1&s=170667a&w=0&k=20&c=Y2xCkfaoHWNVHimBp5d9Ws1IpHKj2k56_E3Js06Jdyo='),
+(10, 7, 'Токио', 990, '2023-03-17', 'https://media.istockphoto.com/id/1131743616/photo/aerial-view-of-tokyo-cityscape-with-fuji-mountain-in-japan.jpg?s=612x612&w=0&k=20&c=0QcSwnyzP__YpBewnQ6_-OZkn0XDtq-mXyvLSSakjZE=');
 
 
 ALTER TABLE `destinations`
@@ -60,4 +62,5 @@ INSERT INTO `dests` (`id`, `name`) VALUES
 (3, 'Барселона'),
 (4, 'Париж'),
 (5, 'Сингапур'),
-(6, 'Венеция');
+(6, 'Венеция'),
+(7, 'Токио');
