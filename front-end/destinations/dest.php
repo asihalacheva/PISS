3545a40d2b496adb7e02eb2bbac595b5ddf68680
stnssllv <106099288+stnssllv@users.noreply.github.com>
2023-01-15 14:31:22 +0200

    update

--- front-end/destinations/dest.php
+++ front-end/destinations/dest.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Destinations</title>
+    <title>Дестинации</title>
     <link href="dest.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script async defer src="../profile/javascript/logout.js"></script>
@@ -16,9 +16,8 @@
         <div class="logo">
           <img src="../../img/logo.webp" class="logo" alt="logo">
         </div>
-
+	<h1><b>Дестинации</b></h1>
           <div class="menu_list">
-            <a href="./dest.php">Дестинации</a>
            <button id="logout" class="lg_btn">Изход</button>
           </div>
       </div>
@@ -115,9 +114,9 @@
                                         foreach($dests_run as $destnames) :
                                             ?>
                                                 <div class="col-md-4 off">
-                                                    <div class="border p-2 boxx">
+                                                    <div class="boxx">
 							<div><img src="<?php echo $destnames['img']; ?>" alt="img" height=240 width=300/></div>
-                                                        <h5><?= $destnames['name_dest']; ?></h5>
+                                                        <h4><?= $destnames['name_dest']; ?></h4>
 							<div>Цена: <?= $destnames['price']; ?> лв.</div>
 							<div>Дата: <?= $destnames['departure']; ?></div>
                                                     </div>
@@ -142,9 +141,9 @@
 				    {
 				    ?>
 					<div class="col-md-4 off">
-						<div class="border p-2 boxx">
+						<div class="boxx">
 							<div><img src="<?= $offers['img']; ?>" alt="img" height=240 width=300/></div>
-							<h5> <?= $offers['name_dest']; ?></h5>
+							<h4> <?= $offers['name_dest']; ?></h4>
 							<div>Цена: <?= $offers['price']; ?> лв.</div>
 							<div>Дата: <?= $offers['departure']; ?></div>
 						</div>
@@ -162,9 +161,9 @@
                                     foreach($dests_run as $destnames) :
                                         ?>
                                             <div class="col-md-4 off">
-                                                <div class="border p-2 boxx">
+                                                <div class="boxx">
 						    <div><img src="<?php echo $destnames['img']; ?>" alt="img" height=240 width=300/></div>
-                                                    <h5><?= $destnames['name_dest']; ?></h5>
+                                                    <h4><?= $destnames['name_dest']; ?></h4>
 						    <div>Цена: <?= $destnames['price']; ?> лв.</div>
 						    <div>Дата: <?= $destnames['departure']; ?></div>
                                                 </div>
@@ -184,7 +183,7 @@
         </div>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+    <!--<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>-->
 </body>
 </html>
