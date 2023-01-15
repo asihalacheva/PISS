25171bdf56d7b9e9fc2a1a711192b4f4503ec7f6
stnssllv <106099288+stnssllv@users.noreply.github.com>
2023-01-15 15:07:02 +0200

    fix title

--- index1.php
+++ index1.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta charset="UTF-8" />
-  <title>TripHelp</title>
+  <title>TripHelp - Начало</title>
   <link href="css/home1_style.css" rel="stylesheet"> 
 </head>
 <body>
